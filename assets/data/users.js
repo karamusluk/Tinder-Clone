2d735c98c33476665a2e6dfd964a311305341843
karamusluk <karamusluk@gmail.com>
2021-08-02 21:05:18 +0300

    App :: users modified

--- assets/data/users.js
+++ assets/data/users.js
@@ -8,28 +8,25 @@ export default [
   {
     id: "2",
     name: "Elon Musk",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png",
+    image: "https://mustafaculban.com/portfolio/avatars/elon.png",
     bio: "A dude with a rocket is looking for a gal with fuel",
   },
   {
     id: "3",
     name: "Zuck",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/zuck.jpeg",
+    image: "https://mustafaculban.com/portfolio/avatars/zuck.jpeg",
     bio: "No need to send me your nudes, I already saw them",
   },
   {
     id: "4",
     name: "Jeffrey Bezos",
-    image:
-      "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/jeff.jpeg",
+    image: "https://mustafaculban.com/portfolio/avatars/jeff.jpeg",
     bio: "CEO, entrepreneur born in 1964, Jeffrey, Jeffrey Bezos",
   },
   {
     id: "5",
     name: "Mustafa Culban",
-    image: "https://mustafaculban.com/portfolio/profile.jpg",
+    image: "https://mustafaculban.com/portfolio/avatars/profile.jpg",
     bio: "Hello there! This needs to be a a bit long text 😉",
   },
 ];
